bc62549c94b10b81fc45415ba9b62afdf3ae49fb
j-IM-bo <75038061+j-IM-bo@users.noreply.github.com>
2021-08-12 23:33:45 +0100

    tweaks

--- src/js/hero.js
+++ src/js/hero.js
@@ -5,6 +5,12 @@ export function initHero() {
         line2 = document.querySelector(".hero__title-mask:nth-child(2) div"),
         copy = document.querySelector(".hero__copy p");
 
-    gsap.from([line1, line2], { duration: 0.6, yPercent: 100, stagger: 0.1 });
-    gsap.from(copy, { duration: 1, autoAlpha: 0 });
+    gsap.set([line1, line2], { yPercent: 100, autoAlpha: 0 });
+    gsap.to([line1, line2], {
+        duration: 0.6,
+        autoAlpha: 1,
+        yPercent: 0,
+        stagger: 0.1
+    });
+    gsap.to(copy, { duration: 1, autoAlpha: 1 });
 }
