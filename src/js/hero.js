1bc659107b98e1948eeed7d6d84f42fe189d3d8e
j-IM-bo <75038061+j-IM-bo@users.noreply.github.com>
2021-08-13 13:16:06 +0100

    footer

--- src/js/hero.js
+++ src/js/hero.js
@@ -5,12 +5,12 @@ export function initHero() {
         line2 = document.querySelector(".hero__title-mask:nth-child(2) div"),
         copy = document.querySelector(".hero__copy p");
 
-    gsap.set([line1, line2], { yPercent: 100, autoAlpha: 0 });
+    // gsap.set([line1, line2], { yPercent: 100, autoAlpha: 0 });
     gsap.to([line1, line2], {
         duration: 0.6,
         autoAlpha: 1,
-        yPercent: 0,
+        y: 0,
         stagger: 0.1
     });
-    gsap.to(copy, { duration: 1, autoAlpha: 1 });
+    gsap.to(copy, { duration: 1.2, autoAlpha: 1 }, "-=0.5");
 }
