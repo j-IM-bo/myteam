1d7293eef43b9a38c25548779afe81f97f28bc51
j-IM-bo <75038061+j-IM-bo@users.noreply.github.com>
2021-08-17 11:51:40 +0100

    about hero done

--- src/js/hero.js
+++ src/js/hero.js
@@ -2,15 +2,12 @@ import { gsap, Power1, Power2, Power3, Power4 } from "gsap";
 
 export function initHero() {
     const line1 = document.querySelector(".hero__title-mask:nth-child(1) div"),
-        line2 = document.querySelector(".hero__title-mask:nth-child(2) div"),
-        copy = document.querySelector(".hero__copy p");
+        line2 = document.querySelector(".hero__title-mask:nth-child(2) div");
 
-    // gsap.set([line1, line2], { yPercent: 100, autoAlpha: 0 });
     gsap.to([line1, line2], {
         duration: 0.6,
         autoAlpha: 1,
         y: 0,
         stagger: 0.1
     });
-    // gsap.to(copy, { duration: 1.2, autoAlpha: 1 }, "-=0.5");
 }
--- src/js/utilities/loadin.js
+++ src/js/utilities/loadin.js
@@ -2,14 +2,12 @@ import { gsap, Power1, Power2, Power3, Power4 } from "gsap";
 
 export function initLoadin() {
     console.log("working");
-    const fadeIn = document.querySelector(".js-fade-in");
 
-    // gsap.set([line1, line2], { yPercent: 100, autoAlpha: 0 });
-    // gsap.to([line1, line2], {
-    //     duration: 0.6,
-    //     autoAlpha: 1,
-    //     y: 0,
-    //     stagger: 0.1
-    // });
-    gsap.to(fadeIn, { autoAlpha: 1 });
+    // copy fade in
+    const fadeIn = document.querySelector(".js-fade-in");
+    gsap.to(fadeIn, { duration: 1, autoAlpha: 1 });
+
+    // title move up
+    const moveUp = document.querySelector(".js-title h1");
+    gsap.to(moveUp, { duration: 0.6, y: 0 });
 }
